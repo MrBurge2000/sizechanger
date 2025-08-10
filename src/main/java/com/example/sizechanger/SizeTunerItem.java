3f040dcbf0dd79c3a1bc80397cff5b99ec07988b
MrBurge <103059875+MrBurge2000@users.noreply.github.com>
2025-08-09 21:03:01 -0700

    Create SizeTunerItem.java

---
+++ src/main/java/com/example/sizechanger/SizeTunerItem.java
@@ -0,0 +1,22 @@
+package com.example.sizechanger;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class SizeTunerItem extends Item {
+    public SizeTunerItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (world.isClient()) {
+            ClientGUI.open(SizeState.get(user));
+        }
+        return TypedActionResult.success(user.getStackInHand(hand), world.isClient());
+    }
+}
